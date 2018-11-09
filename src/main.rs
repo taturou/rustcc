8a440c7627455b5e57d2d239914acac4ff01a2c8
taturou <taturou@gmail.com>
2018-11-09 13:36:21 +0900

    combine クレートを用いてパースするようにした

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
+extern crate combine;
+
 use std::env;
+use combine::parser::char::{spaces, digit};
+use combine::{many1, Parser};
+use combine::stream::easy;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -8,8 +13,24 @@ fn main() {
         std::process::exit(1);
     }
 
-    println!("define i32 @main(i32 %argc, i8** %argv) #0 {{");
-    println!("  ret i32 {}", args[1]);
-    println!("}}");
+    let exp = &args[1];
+
+    let mut integer = spaces()
+                    .with(many1(digit()).map(|str: String| str.parse::<i32>().unwrap()));
+    let result: Result<(i32, &str), easy::ParseError<&str>> = integer.easy_parse(exp);
+
+    match result {
+        Ok((value, "")) => {
+            println!("define i32 @main(i32 %argc, i8** %argv) #0 {{");
+            println!("  ret i32 {}", value);
+            println!("}}");
+        },
+        Ok((value, remain)) => {
+            eprintln!("\"{}\" の後の \"{}\" を認識できません。", value, remain);
+        },
+        Err(err) => {
+            eprintln!("{}", err);
+        }
+    }
     return;
 }
